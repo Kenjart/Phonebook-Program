f570b95576b8de840e0119249c68012618b51eb8
Kenjart <90866887+Kenjart@users.noreply.github.com>
2021-09-20 12:44:19 -0600

    Add files via upload

---
+++ phone_main.py
@@ -0,0 +1,75 @@
+import time
+import phone_module
+a = phone_module.a
+b = phone_module.b
+c = phone_module.c
+d = phone_module.d
+e = phone_module.e
+#module imports and variables
+#TIME IS NOT NECESSARY BUT IT'S FUNNY SO IT'S THERE
+
+#name call
+name = str(input("Hello, please give us your name. "))
+print ("Hello,", name)
+time.sleep(1)
+
+
+#Listing contacts
+print ("Since we are a local company our list of contacts are small.")
+time.sleep(1)
+print ("You can contact")
+time.sleep(1)
+print ("Joe's Mining Company")
+time.sleep(1)
+print ("Joe's Mining Company")
+#THIS IS INTENTIONAL
+time.sleep(1)
+print ("Dave's Gas Company")
+time.sleep(1)
+print ("Steve Woodworking")
+time.sleep(1)
+print ("The Elder Gods which rule us all")
+time.sleep(1)
+
+#Contact choosing
+print ("Please choose the person you would wish to contact")
+phone = float(input("Pick a number from either 1 to 5. "))
+
+#choose your own adventure kind of stuff
+if phone >= 6:
+    time.sleep(2)
+    print ("That is not a valid number.")
+    print ("SYNTAX ERROR AT LINE 400 TRILLION")
+    quit
+
+if phone == 1:
+    time.sleep(1)
+    print (a)
+
+if phone == 2:
+    time.sleep(1)
+    print (b)
+
+if phone == 3:
+    time.sleep(1)
+    print (c)
+
+if phone == 4:
+    time.sleep(1)
+    print (d)
+
+if phone == 5:
+    time.sleep(1)
+    print (e)
+
+time.sleep(10)
+print ("Thank you for using our phonebook.")
+time.sleep(1)
+print ("Have a nice day", name)
+time.sleep(5)
+print ("Oh god they found me it's too late for me")
+time.sleep(1.25)
+print ("run")
+quit
+
+#Final Messages
---
+++ phone_module.py
@@ -0,0 +1,9 @@
+"""
+Module containing phone numbers
+"""
+
+a = ("Joe's Mining Company main contact hotline (mincrft) : 1-800-4206-9133")
+b = ("Sydney's Beauty Inc customer support hotline (not an MLM we swear) : 1-800-1234-5678")
+c = ("Dave's Gas Company secondary contact hotline (ruining the enviroment one day at a time) : 1-800-3928-8738")
+d = ("Steve Woodworking internet and cable hotline (my wife filed for divorce and now I'm sad :( ) : 1-800-8939-7193")
+e = ("░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░ : 1-800-8219-2898")
